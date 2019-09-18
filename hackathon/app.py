ca61d6c8a7ab16d3916be0eb93795fec03a867c1
Denis Bourge <denis.bourge@sharingcloud.com>
2019-09-18 10:43:03 -0500

    feat: update

--- hackathon/app.py
+++ hackathon/app.py
@@ -1,9 +1,11 @@
+import asyncio
 import json
 import re
 
 from flask import Flask, request
 from flask_mqtt import Mqtt
 import requests
+import xows
 
 from . import config
 
@@ -12,8 +14,19 @@ app.config["MQTT_BROKER_URL"] = config.MQTT_BROKER_URL
 app.config["MQTT_BROKER_PORT"] = config.MQTT_BROKER_PORT
 mqtt = Mqtt(app)
 
+loop = asyncio.get_event_loop()
+
 MQTT_TOPIC_RGX = re.compile(r"/merakimv/(?P<serial>[0-9A-Z-]+)/raw_detections")
 CAMERA_STATE = {}
+
+###########
+# Utilities
+
+async def fetch_info():
+    async with xows.XoWSClient('10.89.130.68', 'cisco', 'cisco') as client:
+        data = await client.xCommand(['Message', 'Send'], Text='Hello from Python')
+        print(data)
+        # await client.wait_until_closed()
 
 #############
 # MQTT routes
@@ -36,8 +49,8 @@ def handle_message(client, userdata, message):
         current_persons_count = len(persons)
         previous_persons_count = CAMERA_STATE.get(camera_serial, 0)
 
-        if current_persons_count != previous_persons_count:
-            print(f"There are now {current_persons_count} people on camera {camera_serial}")
+        # if current_persons_count != previous_persons_count:
+            # print(f"There are now {current_persons_count} people on camera {camera_serial}")
 
         # Update people count
         CAMERA_STATE[camera_serial] = current_persons_count
@@ -48,4 +61,9 @@ def handle_message(client, userdata, message):
 @app.route('/json', methods=['POST'])
 def json_test():
     print(request.get_json())
+    return "ok"
+
+@app.route('/xows-test')
+def xows_test():
+    data = loop.run_until_complete(fetch_info())
     return "ok"
